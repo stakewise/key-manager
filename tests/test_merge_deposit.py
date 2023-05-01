d7ca4c03c37122b5b7c148ecf74704c614704279
antares-sw <23400824+antares-sw@users.noreply.github.com>
2023-05-01 13:54:13 +0400

    Fix

--- tests/test_merge_deposit.py
+++ tests/test_merge_deposit.py
@@ -7,17 +7,17 @@ from key_manager.commands.merge_deposit import merge_deposit_files
 
 def test_merge_deposit_files():
     file1_content = [
-        {"id": 1, "pubkey": "0x1"},
-        {"id": 3, "pubkey": "0x3"},
+        {'id': 1, 'pubkey': '0x1'},
+        {'id': 3, 'pubkey': '0x3'},
     ]
     file2_content = [
-        {"id": 2, "pubkey": "0x2"},
-        {"id": 4, "pubkey": "0x4"},
+        {'id': 2, 'pubkey': '0x2'},
+        {'id': 4, 'pubkey': '0x4'},
     ]
 
     with tempfile.NamedTemporaryFile('w', delete=False) as file1, \
-        tempfile.NamedTemporaryFile('w', delete=False) as file2, \
-        tempfile.NamedTemporaryFile('w', delete=False) as merged_file:
+            tempfile.NamedTemporaryFile('w', delete=False) as file2, \
+            tempfile.NamedTemporaryFile('w', delete=False) as merged_file:
         json.dump(file1_content, file1)
         json.dump(file2_content, file2)
 
@@ -30,10 +30,10 @@ def test_merge_deposit_files():
             merged_json = json.load(result)
 
         expected_merged_json = [
-            {"id": 1, "pubkey": "0x1"},
-            {"id": 2, "pubkey": "0x2"},
-            {"id": 3, "pubkey": "0x3"},
-            {"id": 4, "pubkey": "0x4"},
+            {'id': 1, 'pubkey': '0x1'},
+            {'id': 2, 'pubkey': '0x2'},
+            {'id': 3, 'pubkey': '0x3'},
+            {'id': 4, 'pubkey': '0x4'},
         ]
 
         assert merged_json == expected_merged_json
