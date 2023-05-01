52ab811c849d1c5554c81b985e4c81df8e4f0091
antares-sw <23400824+antares-sw@users.noreply.github.com>
2023-05-01 15:14:17 +0400

    Add merge deposit data command (#28)
    * Add merge deposit data command
    
    * Fix linting
    
    * Fix
    
    * Rewrite tests
    
    * Ref
    
    * Fix test

--- key_manager/main.py
+++ key_manager/main.py
@@ -5,6 +5,7 @@ import click
 from key_manager.commands.create_keys import create_keys
 from key_manager.commands.create_mnemonic import create_mnemonic
 from key_manager.commands.create_wallet import create_wallet
+from key_manager.commands.merge_deposit import merge_deposit
 from key_manager.commands.sync_validator import sync_validator
 from key_manager.commands.sync_web3signer import sync_web3signer
 from key_manager.commands.update_db import update_db
@@ -18,6 +19,7 @@ def cli() -> None:
 cli.add_command(create_mnemonic)
 cli.add_command(create_keys)
 cli.add_command(create_wallet)
+cli.add_command(merge_deposit)
 cli.add_command(update_db)
 cli.add_command(sync_web3signer)
 cli.add_command(sync_validator)
